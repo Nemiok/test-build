cf0b7cc595a2296de9d1e202b499d10baa432f23
Oktai Nematov <onematov@docdoc.group>
2024-07-30 18:07:22 +0500

    6

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    if ('OTPCredential' in window) {
+      window.addEventListener('DOMContentLoaded', () => {
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const ac = new AbortController();
+        const form = input.closest('form');
+        if (form) {
+          form.addEventListener('submit', () => {
+            ac.abort();
+          });
+        }
+        navigator.credentials.get({
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          otp: { transport:['sms'] },
+          signal: ac.signal
+        }).then(otp => {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          input.value = otp.code;
+          if (form) form.submit();
+        }).catch(err => {
+          console.log(err);
+        });
+      });
+    }
+  },[])
 
   return (
     <div>
       <form >
         <input autoComplete="one-time-code" inputMode="numeric" type="text" />
-
       </form>
     </div>  
   )
