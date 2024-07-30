be219bb50465089bde2523ff5528b9190b95c337
Oktai Nematov <onematov@docdoc.group>
2024-07-30 17:43:59 +0500

    vite try

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <form >
+        <input autoComplete="one-time-code" inputMode="numeric" type="text" />
+
+      </form>
+    </div>  
+  )
+}
+
+export default App
